59f7969a587c1b7efcf9dbf22fee87c14960e8e9
Legoshii <youtune.contact.rageman@gmail.com>
2023-04-11 19:46:00 +0200

    Update

--- main.py
+++ main.py
@@ -1,27 +1,74 @@
 import subprocess
-import tqdm
 import time
 import os
-from tqdm import tqdm
+import urllib.request
+import importlib
 
+# Définition pour voir si `pipp` et que `tqdm` sont installer
+def is_pip_installed():
+    try:
+        importlib.import_module('pip')
+        return True
+    except ImportError:
+        return False
+    
+def is_tqdm_installed():
+    try:
+        importlib.import_module('tqdm')
+        return True
+    except ImportError:
+        return False
+
+# Stock de l'url de la dernière version du logiciel
+url = "https://archive.legoshii.fr/Multi_Tools/download/main.py"
+
+# Définition pour installer les indépendances
+def install_dep():
+    print("Téléchargement des modules...")
+    os.system("pip install tqdm")
+    os.system("cls")
+    from tqdm import tqdm
+
+# Test pour savoir si `pip` et `tqdm` sont installer
+if is_pip_installed() and is_tqdm_installed():
+    print("pip et le module tqdm sont déjà installés.")
+    os.system("cls")
+else:
+    if not is_pip_installed():
+        print("pip n'est pas installé. Installation en cours...")
+        subprocess.check_call(['python', '-m', 'ensurepip', '--default-pip'])
+        print("pip a été installé avec succès.")
+        os.system("cls")
+        install_dep()
+
+    if not is_tqdm_installed():
+        print("Le module tqdm n'est pas installé. Installation en cours...")
+        subprocess.check_call(['pip', 'install', 'tqdm'])
+        print("Le module tqdm a été installé avec succès.")
+        os.system("cls")
+
+# Mise à jour automatique
 stats = os.stat('main.py')
-if stats == stats:
-    for i in tqdm(range(0, 1023), total = 1023,
+#print(stats..st_size, "octets") #Affichage de la taille du fichier
+"""if stats != 3579: #Si le fichier n'est pas égale à la valeur de la taille du fichier de la dernière mise à jour alors:
+    for i in tqdm(range(0, 3579), total = 3 579, #3 579 ici = Taille du fichier de la dernière mise à jour
                 desc ="Téléchargement de la mise à jour"):
-        time.sleep(0.00000000000000000000009)
+        time.sleep(.1)
     time.sleep(1)
+    urllib.request.urlretrieve(url, "main.py")
     print("Installation de la mise à jour...")
     time.sleep(3)
-    resultCLS = subprocess.run(['cls'], capture_output=True, shell=True)
-    print(cls)
+    os.system("cls")
 else :
     None
+"""
 
+#Début du logiciel
 os.system("title Multi Tools V1 - Version 0.1")
 
 def num_class():
     print("Multi Tools V1 - Made By Legoshii レゴシイ#3660\n")
-    time.sleep(2)
+    time.sleep(1)
 
     choix_fonction = input("""
 [0] Informations du logiciel
@@ -35,9 +82,8 @@ def num_class():
 Choix > """)
 
     if choix_fonction == '1':
-        print("Veuillez patienter pendant que nous éxecutons la commande ...")
-        result = subprocess.run(['winget', 'upgrade', '--all'], capture_output=True)
-        print(result.stdout.decode('utf-8'))
+        os.system("winget upgrade --all")
+        next()
     elif choix_fonction == '0':
         print(""" 
 - Version [0.1]
@@ -46,20 +92,34 @@ Choix > """)
 - Site web : https://legoshii.fr
 - Support : SOON
         """)
+        next()
     elif choix_fonction == '2':
-        result = subprocess.run(['ipconfig'], capture_output=True)
-        print(result.stdout.decode('utf-8'))
+        os.system("ipconfig")
+        next()
     elif choix_fonction == '3':
-        result = subprocess.run(['ipconfig', '/flushdns'], capture_output=True)
-        print(result.stdout.decode('utf-8'))
+        os.system("ipconfig /flushdns")
+        next()
     elif choix_fonction == '4':
-        result = subprocess.run(['systeminfo'], capture_output=True)
-        print(result.stdout.decode('utf-8'))
+        os.system("systeminfo")
+        next()
     elif choix_fonction == '69':
         print("`\nNice :)")
+        next()
     elif choix_fonction == '99':
-        result = subprocess.run(['exit'], capture_output=True, shell=True)
+        os.system("exit")
     else:
         print("Choix invalide")
+
+def next():
+    time.sleep(1)
+    choix_next = input("""Continuer ? (Y/N) """)
+    if choix_next == "Y":
+        os.system("cls")
+        num_class()
+    if choix_next == "N":
+        next()
+    else:
+        print("Choix invalide :/")
+        next()
 
 num_class()
--- start.bat
+++ start.bat
@@ -3,4 +3,3 @@ REM python.exe -m pip install --upgrade pip
 REM py -3 -m pip install -r requirements.txt
 cls
 python main.py
-pause
